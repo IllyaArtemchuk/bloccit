19b90dadb9fddebf193689ac44603c20bdb0e3ad
Illya Artemchuk <ilyalinkis@gmail.com>
2018-11-10 16:13:18 -0800

    Assignment 1 completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -11,4 +11,13 @@ describe("routes : static", ()=> {
             });
         });
     });
+    describe("GET /marco", ()=> {
+        it("should return status code 200 and a body of polo", (done)=>{
+            request.get(base + "marco", (err, res, body)=> {
+                expect(res.statusCode).toBe(200);
+                expect(body).toBe("polo");
+                done();
+            })
+        })
+    })
 });
--- src/routes/static.js
+++ src/routes/static.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const router = express.router();
+const router = express.Router();
 
 router.get("/", (req, res, next)=> {
     res.send("welcome to bloccit");
 });
+
+router.get("/marco", (req,res,next)=> {
+    res.send("polo")
+})
 
 module.exports = router;
 
